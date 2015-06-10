dcd8580e12f150d1419a77a42a31c5ceb44ba863
Ahmed Alnaqaa <ahmedalnaqaa@gmail.com>
2015-06-10 16:41:47 +0200

    Configure MongoDB

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Blog\Bundle\MainBundle\BlogMainBundle(),
             new Blog\Bundle\UserBundle\BlogUserBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
